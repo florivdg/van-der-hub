cc2fec4e95e09ec1172d3ff1a77224687c1b7d27
Florian van der Galiën <hallo@flori.dev>
2023-03-23 23:28:04 +0100

    Added a POST endpoint for updating

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import WebSocket, { WebSocketServer } from 'ws'
+import Fastify from 'fastify'
 
 /**
  * The WebSocket server.
  */
-const wss = new WebSocketServer({ port: 8787 })
+const wss = new WebSocketServer({ path: '/live', port: 8787 })
 
 /**
  * Use a proxy to set the browser name and trigger side effects.
@@ -15,7 +16,8 @@ const browser = new Proxy(
   {
     set: (target, prop, value) => {
       if (prop === 'value') {
-        target.value = value
+        /// Don't do anything if the value hasn't changed
+        if (target.value === value) return true
 
         /// Send browser name to all clients
         wss.clients.forEach(function each(client) {
@@ -23,6 +25,9 @@ const browser = new Proxy(
             client.send(value)
           }
         })
+
+        /// Set the internal value
+        target.value = value
       }
 
       return true
@@ -46,3 +51,25 @@ wss.on('connection', function connection(ws) {
 setTimeout(() => {
   browser.value = 'com.google.Chrome'
 }, 10000)
+
+/**
+ * The Fastify server.
+ */
+const fastify = Fastify({
+  logger: false,
+})
+
+/**
+ * POST /set
+ * Set the browser name.
+ */
+fastify.post<{ Body: { browser: string } }>('/set', async (request, reply) => {
+  /// TODO: Auth
+  browser.value = request.body.browser
+})
+
+// Run the server!
+fastify.listen({ port: 8686 }, (err, address) => {
+  if (err) throw err
+  // Server is now listening on ${address}
+})
