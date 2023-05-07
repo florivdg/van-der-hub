c8df34c583efe56eafa76f2e1b52499ebb504fcc
Florian van der Galiën <hallo@flori.dev>
2023-05-07 04:34:02 +0200

    Upgraded deps

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { serve } from 'https://deno.land/std@0.185.0/http/server.ts'
-import 'https://deno.land/std@0.185.0/dotenv/load.ts'
+export { serve } from 'https://deno.land/std@0.186.0/http/server.ts'
+import 'https://deno.land/std@0.186.0/dotenv/load.ts'
